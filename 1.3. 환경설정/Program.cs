46b076d1656226435bdad72a29c5a78fc31c66b4
Jihoon Song <spec0003@naver.com>
2022-05-13 15:51:40 +0900

    Commit "1.3. 환경 설정" final code

--- 1.3. 환경설정/Program.cs
+++ 1.3. 환경설정/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,34 @@ namespace _1._3.환경설정
     {
         static void Main(string[] args)
         {
+            Console.CursorVisible = false;
+
+            const int WAIT_TICK = 1000 / 30;
+            const char CIRCLE = '\u25cf';
+
+            int lastTick = 0;
+            while (true)
+            {
+                #region 프레임 관리
+                // 만약에 경과한 시간이 1/30초보다 작다면
+                int currentTick = System.Environment.TickCount;
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                lastTick = currentTick;
+                #endregion
+
+                Console.SetCursorPosition(0, 0);
+                
+                for (int i = 0; i < 25; i++)
+                {
+                    for (int j = 0; j < 25; j++)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(CIRCLE);
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
