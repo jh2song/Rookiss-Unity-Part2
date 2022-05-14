42c5ae22c5d2edd14989192f4437fc1a581654d3
Jihoon Song <spec0003@naver.com>
2022-05-15 04:37:55 +0900

    Commit "2.3. 연결 리스트 구현 연습" final code

--- 1.3. 환경설정/Board.cs
+++ 1.3. 환경설정/Board.cs
@@ -6,45 +6,59 @@ using System.Threading.Tasks;
 
 namespace _1._3.환경설정
 {
-    class MyList<T>
+    class MyLinkedListNode<T>
     {
-        const int DEFAULT_SIZE = 1;
-        T[] _data = new T[DEFAULT_SIZE];
+        public T Data;
+        public MyLinkedListNode<T> Next;
+        public MyLinkedListNode<T> Prev;
+    }
 
-        public int Count = 0; // 실제로 사용중인 데이터 개수
-        public int Capacity { get { return _data.Length; } } // 예약된 데이터 개수
+    class MyLinkedList<T>
+    {
+        public MyLinkedListNode<T> Head = null; // 첫번째
+        public MyLinkedListNode<T> Tail = null; // 마지막
+        public int Count = 0;
 
         // O(1)
-        public void Add(T item)
+        public MyLinkedListNode<T> AddLast(T data)
         {
-            // 1. 공간이 충분히 남아 있는지 확인한다
-            if (Count >= Capacity)
+            MyLinkedListNode<T> newNode = new MyLinkedListNode<T>();
+            newNode.Data = data;
+
+            // 만약에 아직 방이 아예 없었다면 새로 추가된 첫번째 방이 곧 Head이다.
+            if (Head == null)
+                Head = newNode;
+
+            // 기존의 [마지막 방]과 [새로 추가되는 방]을 연결해준다.
+            if (Tail != null)
             {
-                // 공간을 다시 늘려서 확보한다
-                T[] newArray = new T[Count * 2];
-                for (int i = 0; i < Count; i++)
-                    newArray[i] = _data[i];
-                _data = newArray;
+                Tail.Next = newNode;
+                newNode.Prev = Tail;
             }
 
-            // 2. 공간에다가 데이터를 넣어준다
-            _data[Count] = item;
+            // [새로 추가되는 방]을 [마지막 방]으로 인정한다.
+            Tail = newNode;
             Count++;
-        }
-        
-        // O(1)
-        public T this[int index]
-        {
-            get { return _data[index]; }
-            set { _data[index] = value; }
+            return newNode;
         }
 
-        // O(N)
-        public void RemoveAt(int index)
+        // O(1)
+        public void Remove(MyLinkedListNode<T> node)
         {
-            for (int i = index; i < Count - 1; i++)
-                _data[i] = _data[i + 1];
-            _data[Count - 1] = default(T);
+            // [기존의 첫번째 방의 다음 방]을 [첫번째 방으로] 인정한다.
+            if (Head == node)
+                Head = Head.Next;
+
+            // [기존의 마지막 방의 이전 방]을 [마지막 방으로] 인정한다.
+            if (Tail == node)
+                Tail = Tail.Prev;
+
+            if (node.Prev != null)
+                node.Prev.Next = node.Next;
+
+            if (node.Next != null)
+                node.Next.Prev = node.Prev;
+
             Count--;
         }
     }
@@ -52,21 +66,17 @@ namespace _1._3.환경설정
     class Board
     {
         public int[] _data = new int[25]; // 배열
-        public MyList<int> _data2 = new MyList<int>(); // 동적 배열
-        public LinkedList<int> _data3 = new LinkedList<int>();
+        public MyLinkedList<int> _data3 = new MyLinkedList<int>(); // 연결 리스트
 
         public void Initialize()
         {
-            _data2.Add(101);
-            _data2.Add(102);
-            _data2.Add(103);
-            _data2.Add(104);
-            _data2.Add(105);
+            _data3.AddLast(101);
+            _data3.AddLast(102);
+            MyLinkedListNode<int> node = _data3.AddLast(103);
+            _data3.AddLast(104);
+            _data3.AddLast(105);
 
-            int temp = _data2[2];
-            _data2[2] = 3;
-
-            _data2.RemoveAt(2);
+            _data3.Remove(node);
         }
     }
 }
