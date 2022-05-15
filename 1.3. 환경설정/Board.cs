37635cbd542897308f9f3dd6eb621ef6bb2617f7
Jihoon Song <spec0003@naver.com>
2022-05-16 08:51:58 +0900

    Commit "3.2. Binary Tree 미로 생성 알고리즘" final code

--- 1.3. 환경설정/Board.cs
+++ 1.3. 환경설정/Board.cs
@@ -20,17 +20,63 @@ namespace _1._3.환경설정
 
         public void Initialize(int size)
         {
+            if (size % 2 == 0)
+                return;
+
             _tile = new TileType[size, size];
             _size = size;
 
+            // Mazes for Programmers: 책
+            GenerateByBinaryTree();
+        }
+
+        void GenerateByBinaryTree()
+        {
+            // 일단 길을 다 막아버리는 작업
             for (int y = 0; y < _size; y++)
             {
                 for (int x = 0; x < _size; x++)
                 {
-                    if (x == 0 || x == size - 1 || y == 0 || y == size - 1)
+                    if (x % 2 == 0 || y % 2 == 0)
                         _tile[y, x] = TileType.Wall;
                     else
                         _tile[y, x] = TileType.Empty;
+                }
+            }
+
+            // 랜덤으로 우측 혹은 아래로 길을 뚫는 작업
+            // Binary Tree Algorithm
+            Random rand = new Random();
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue;
+
+                    if (y == _size - 2 && x == _size - 2)
+                        continue;
+
+                    if (y == _size - 2)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (x == _size - 2)
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                    }
+                    else
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                    }
                 }
             }
         }
