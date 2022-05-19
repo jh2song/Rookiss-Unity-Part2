e7b7438a52db9a4d22583413871e24e6e7b3ba36
Jihoon Song <spec0003@naver.com>
2022-05-19 13:12:43 +0900

    Commit "~4.6. BFS를 이용한 길찾기 구현"

--- Algorithm/Player.cs
+++ Algorithm/Player.cs
@@ -37,6 +37,65 @@ namespace Algorithm
 			PosX = posX;
 			_board = board;
 
+			// 우수법
+			// RightHand();
+
+			BFS();
+		}
+
+		public void BFS()
+        {
+			int[] deltaY = new int[] { -1, 0, 1, 0 };
+			int[] deltaX = new int[] { 0, -1, 0, 1 };
+
+			bool[,] found = new bool[_board.Size, _board.Size];
+			Pos[,] parent = new Pos[_board.Size, _board.Size];
+
+			Queue<Pos> q = new Queue<Pos>();
+			q.Enqueue(new Pos(PosY, PosX));
+			found[PosY, PosX] = true;
+			parent[PosY, PosX] = new Pos(PosY, PosX);
+
+			while (q.Count > 0)
+            {
+				Pos pos = q.Dequeue();
+				int nowY = pos.Y;
+				int nowX = pos.X;
+
+				for (int i = 0; i < 4; i++)
+                {
+					int nextY = nowY + deltaY[i];
+					int nextX = nowX + deltaX[i];
+
+					if (nextX < 0 || nextX >= _board.Size || nextY < 0 || nextY >= _board.Size)
+						continue;
+					if (_board.Tile[nextY, nextX] == Board.TileType.Wall)
+						continue;
+					if (found[nextY, nextX])
+						continue;
+
+					q.Enqueue(new Pos(nextY, nextX));
+					found[nextY, nextX] = true;
+					parent[nextY, nextX] = new Pos(nowY, nowX);
+                }
+            }
+
+			int y = _board.DestY;
+			int x = _board.DestX;
+			while (parent[y, x].Y != y || parent[y, x].X != x)
+            {
+				_points.Add(new Pos(y, x));
+				Pos pos = parent[y, x];
+				y = pos.Y;
+				x = pos.X;
+            }
+			_points.Add(new Pos(y, x));
+			_points.Reverse();
+        }
+
+		public void RightHand()
+        {
+
 			// 현재 바라보고 있는 방향을 기준으로, 좌표 변화를 나타낸다
 			int[] frontY = new int[] { -1, 0, 1, 0 };
 			int[] frontX = new int[] { 0, -1, 0, 1 };
@@ -45,7 +104,7 @@ namespace Algorithm
 
 			_points.Add(new Pos(PosY, PosX));
 			// 목적지 도착하기 전에는 계속 실행
-			while (PosY != board.DestY || PosX != board.DestX)
+			while (PosY != _board.DestY || PosX != _board.DestX)
 			{
 				// 1. 현재 바라보는 방향을 기준으로 오른쪽으로 갈 수 있는지 확인.
 				if (_board.Tile[PosY + rightY[_dir], PosX + rightX[_dir]] == Board.TileType.Empty)
